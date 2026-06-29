aac48c64f78f0008c4482140dd43779c4abe5cc3
Bruno Galvagni <bruno.galvagni21@gmail.com>
2026-06-28 21:42:31 -0300

    fix: Alteração dos produtos

---
+++ product-service/src/main/resources/db_migration/V2__PopulateTableTbProduct.sql
@@ -0,0 +1,9 @@
+INSERT INTO tb_product (description, brand, model, currency, price, stock) VALUES
+                                                   ('Technotronic', 'Disco', 'The Greatest Hits', 'USD', 59.99, 1),
+                                                   ('Meddle', 'Disco', 'Pink Floyd', 'USD', 100.00, 1),
+                                                   ('Vulgar Display of Power', 'Disco', 'Pantera', 'USD', 40.00, 1),
+                                                   ('Kill ''Em All', 'Disco', 'Metallica', 'USD', 1299.00, 1),
+                                                   ('Um Guia à Bruxaria Tradicional: A Vassoura, o Cajado e a Espada', 'Livro', 'Livro', 'USD', 160.00, 1),
+                                                   ('Hamlet', 'Livro', 'William Shakespeare', 'USD', 30.00, 1),
+                                                   ('A Trilogia da Fonte do Sol', 'Livro', 'Warcraft', 'USD', 100.00, 1),
+                                                    ('A Coisa', 'Livro', 'Stephen King', 'USD', 179.00, 1)
